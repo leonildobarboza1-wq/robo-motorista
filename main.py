f7f3423e49bb5049c1dd037b81ce40e7f98066d7
Leonildo Barboza <leonildobarboza1@gmail.com>
2026-06-02 12:18:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# ==========================================
+# ========================================== 
 # CONFIGURAÇÕES E AMBIENTE
 # ==========================================
 BLOG_ID = os.environ.get("BLOG_ID")
